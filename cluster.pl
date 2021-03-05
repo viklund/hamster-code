458dbb8ca99e0edcddfba9267d9b7f9ae6562032
Johan Viklund <johan.viklund@nbis.se>
2021-03-05 21:02:16 +0100

    Optimize pass one, calculate with epochs instead

--- cluster.pl
+++ cluster.pl
@@ -30,30 +30,29 @@ while (<$HAMSTER>) {
         time_zone => 'floating',
     );
 
+    $date = $date->epoch + $millis/1_000_000;
+
     if ( ! @current ) {
         push @current, $date;
         next;
     }
-    if ( cdiff( $date, $current[$#current] ) <= $CUTOFF ) {
+    if ( $date - $current[$#current] <= $CUTOFF ) {
         push @current, $date;
         next;
     }
     if ( @current ) {
         if ( @current > 1 ) {
             my $diff = $current[-1] - $current[0];
-            my @d =  $diff->in_units('days', 'minutes', 'nanoseconds');
-            $current[0]->add(days => $d[0]/2, minutes => $d[1]/2, nanoseconds => $d[2]/2);
+            $current[0] += $diff/2;
         }
         push @times, $current[0];
-        #say $current[0]->strftime('%FT%T.%N');
         @current = ($date);
     }
 } 
 if ( @current ) {
     if ( @current > 1 ) {
         my $diff = $current[-1] - $current[0];
-        my @d =  $diff->in_units('days', 'minutes', 'nanoseconds');
-        $current[0]->add(days => $d[0]/2, minutes => $d[1]/2, nanoseconds => $d[2]/2);
+        $current[0] += $diff/2;
     }
     push @times, $current[0];
     @current = ();
@@ -75,7 +74,7 @@ for my $t (@times) {
         $prev = $start = $t;
         next;
     }
-    my $cdiff = cdiff( $t, $current[-1] );
+    my $cdiff = $t - $current[-1];
     if ( $cdiff <= $SESSION_CUTOFF ) {
         push @current, $t;
         $prev = $t;
@@ -85,7 +84,7 @@ for my $t (@times) {
         printf "Runner: %6.2f km (%5d %5.2f) %s -- %s\n",
             $tot_laps * 3.14 * 0.2 / 1000,
             $tot_laps, $tot_seconds/3600,
-            $start->iso8601, $prev->iso8601;
+            DateTime->from_epoch(epoch => $start)->iso8601, DateTime->from_epoch(epoch => $prev)->iso8601;
         $tot_laps = 0;
         $tot_seconds = 0;
         $start = $t;
@@ -93,7 +92,7 @@ for my $t (@times) {
     elsif ( @current > 1 ) {
         my $laps = @current - 0.5;
         $tot_laps += $laps;
-        my $s = cdiff($current[$#current], $current[0]);
+        my $s = $current[$#current] - $current[0];
         $tot_seconds += $s;
     }
     @current = ($t);
@@ -103,23 +102,4 @@ for my $t (@times) {
 printf "Runner: %6.2f km (%5d %5.2f) %s -- %s\n",
     $tot_laps * 3.14 * 0.2 / 1000,
     $tot_laps, $tot_seconds/3600,
-    $start->iso8601, $prev->iso8601;
-#printf "Naive calculation: %6.2f km\n", scalar(@times) * 3.14 * 0.2 / 1000;
-#printf "Smart calculation: %6.2f km\n", $tot_laps * 3.14 * 0.2 / 1000;
-
-#say $tot_laps;
-#say $tot_seconds;
-#say $tot_laps / $tot_seconds;
-
-sub cdiff {
-    my ($dt1, $dt2) = @_;
-    my $diff = $dt1 - $dt2;
-    my @diff = $diff->in_units('days', 'minutes', 'nanoseconds');
-    my @weight = (60*60*24, 60, 10**-9);
-    my $seconds = sum map { $diff[$_]*$weight[$_] } 0..$#diff;
-    return $seconds;
-}
-
-for my $t ( @times ) {
-
-}
+    DateTime->from_epoch(epoch => $start)->iso8601, DateTime->from_epoch(epoch => $prev)->iso8601;
