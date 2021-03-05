5da7cf0f7e2161ad5256a51b8ae337f0343c12b9
Johan Viklund <johan.viklund@nbis.se>
2021-03-05 20:43:29 +0100

    Button detector

---
+++ test-button.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import sys
+from gpiozero import Button
+from signal import pause
+from datetime import datetime
+
+button = Button(2)
+
+num = 0
+def count():
+    global num
+    if button.is_pressed:
+        num += 1
+        now = datetime.now()
+        print("{}  {:5}".format(now, num))
+        sys.stdout.flush()
+
+
+button.when_pressed = count
+
+print("READY")
+sys.stdout.flush()
+pause()
