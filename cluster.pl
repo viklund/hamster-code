db6e58bf6f74f11ac5d3931f36bd03b4df4ddb63
Johan Viklund <johan.viklund@nbis.se>
2021-03-05 21:29:17 +0100

    Optimize pass four, unpack instead of regex

--- cluster.pl
+++ cluster.pl
@@ -14,10 +14,11 @@ my $CUTOFF = 0.34; # Valley from stats
 
 my @times = ();
 my @current = ();
+<$HAMSTER>;
 while (<$HAMSTER>) {
     # 2021-03-04 06:32:28.177878  35520   
-    next if /^READY/;
-    my ($time, $millis, $count) = /^([^.]+)\.(\d+)\s+(\d+)\s*$/;
+    #my ($time, $millis, $count) = /^([^.]+)\.(\d+)\s+(\d+)\s*$/;
+    my ($time, $millis) = unpack("A19xA6",$_);
 
     #my $date = Time::Piece->strptime($time, '%Y-%m-%d %H:%M:%S')->epoch + $millis/1_000_000;
     my $date = my_epoch_parser( $time ) + $millis/1_000_000;
@@ -99,7 +100,9 @@ printf "Runner: %6.2f km (%5d %5.2f) %s -- %s\n",
 sub my_epoch_parser {
     my $time = shift;
     state $epoch_base;
-    my ($year, $month, $day, $hour, $minute, $second) = $time =~ /(\d+)-(\d+)-(\d+) (\d+):(\d+):(\d+)$/;
+    #my ($year, $month, $day, $hour, $minute, $second) = $time =~ /(\d+)-(\d+)-(\d+) (\d+):(\d+):(\d+)$/;
+    my ($year, $month, $day, $hour, $minute, $second) = unpack("A4xA2xA2xA2xA2xA2",$time);
+
     if ( !ref($epoch_base) || $epoch_base->{month} != $month) {
         my $start = Time::Piece->strptime("$year-$month-01 00:00:00", '%Y-%m-%d %H:%M:%S');
         $epoch_base = {
