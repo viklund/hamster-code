d0efe2c705dfdbb6fb66bc91c0a01744f0d05000
Johan Viklund <johan.viklund@nbis.se>
2021-03-06 17:46:27 +0100

    Log to file instead

--- log-hamster-wheel.py
+++ log-hamster-wheel.py
@@ -1,21 +1,30 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 import sys
-from gpiozero import Button
+from os import path
 from signal import pause
+import time
+
+from gpiozero import Button
 from datetime import datetime
 
 button = Button(2)
 
+filename_pattern = "logs/hamster-spinner-%Y%m%d%H%M%S.log"
+
+filename = datetime.now().strftime(filename_pattern)
+while path.isfile(filename):
+    time.sleep(1)
+    filename = datetime.now().strftime(filename_pattern)
+
+# Open file without buffering, in case power gets cut for some reason.
+fh = open(filename, 'w', buffering=0)
+
 def count():
     if button.is_pressed:
         now = datetime.now()
-        print(now.timestamp())
-        sys.stdout.flush()
-
+        fh.write(now.strftime("%s.%f\n"))
 
 button.when_pressed = count
 
-sys.stderr.write("READY\n")
-sys.stdout.flush()
 pause()
