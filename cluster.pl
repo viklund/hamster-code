90dfa8b502c41d0ab690c8ab289ad1447b76c65e
Johan Viklund <johan.viklund@nbis.se>
2021-03-06 18:09:26 +0100

    Simplify cluster to not do any time parsing

--- cluster.pl
+++ cluster.pl
@@ -2,42 +2,39 @@
 use strict;
 use warnings;
 
-use feature qw/ say state /;
+use feature qw/ say /;
 use Time::Piece;
 
 use List::Util qw/ sum /;
-
-open my $HAMSTER, '<', 'hamster-spinner.log' or die;
 
 my $CUTOFF = 0.34; # Valley from stats
 
 my @times = ();
 my @current = ();
-while (<$HAMSTER>) {
-    # 2021-03-04 06:32:28.177878  35520   
-    #my ($time, $millis, $count) = /^([^.]+)\.(\d+)\s+(\d+)\s*$/;
-    my ($time, $millis) = unpack("A19xA6",$_);
 
-    #my $date = Time::Piece->strptime($time, '%Y-%m-%d %H:%M:%S')->epoch + $millis/1_000_000;
-    my $date = my_epoch_parser( $time ) + $millis/1_000_000;
+for my $file (glob("logs/*.log")) {
+    open my $HAMSTER, '<', $file or die "Could not open $file: $!, $?";
+    while (my $date = <$HAMSTER>) {
+        chomp($date);
 
-    if ( ! @current ) {
-        push @current, $date;
-        next;
-    }
-    if ( $date - $current[$#current] <= $CUTOFF ) {
-        push @current, $date;
-        next;
-    }
-    if ( @current ) {
-        if ( @current > 1 ) {
-            my $diff = $current[-1] - $current[0];
-            $current[0] += $diff/2;
+        if ( ! @current ) {
+            push @current, $date;
+            next;
         }
-        push @times, $current[0];
-        @current = ($date);
+        if ( $date - $current[$#current] <= $CUTOFF ) {
+            push @current, $date;
+            next;
+        }
+        if ( @current ) {
+            if ( @current > 1 ) {
+                my $diff = $current[-1] - $current[0];
+                $current[0] += $diff/2;
+            }
+            push @times, $current[0];
+            @current = ($date);
+        }
     }
-} 
+}
 if ( @current ) {
     if ( @current > 1 ) {
         my $diff = $current[-1] - $current[0];
@@ -46,6 +43,7 @@ if ( @current ) {
     push @times, $current[0];
     @current = ();
 }
+
 
 ### Since it most probably starts with the weight down, the first instance is a
 ### half lap. So we need to detect every "session" and then for every session
@@ -96,22 +94,3 @@ printf "%6.2f km (%5d %2d:%2d)  |  %s -- %s\n",
     $tot_laps, $hours, ($tot_seconds-$hours*3600)/60,
     Time::Piece->strptime(int($start), '%s')->datetime,
     Time::Piece->strptime(int($prev), '%s')->datetime;
-
-
-
-sub my_epoch_parser {
-    my $time = shift;
-    state $epoch_base;
-    #my ($year, $month, $day, $hour, $minute, $second) = $time =~ /(\d+)-(\d+)-(\d+) (\d+):(\d+):(\d+)$/;
-    my ($year, $month, $day, $hour, $minute, $second) = unpack("A4xA2xA2xA2xA2xA2",$time);
-
-    if ( !ref($epoch_base) || $epoch_base->{month} != $month) {
-        my $start = Time::Piece->strptime("$year-$month-01 00:00:00", '%Y-%m-%d %H:%M:%S');
-        $epoch_base = {
-            year => $year,
-            month => $month,
-            base => $start->epoch,
-        }
-    }
-    return $epoch_base->{base} + ($day-1)*86400 + $hour*3600 + $minute*60 + $second;
-}
