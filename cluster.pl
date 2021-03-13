75f20ced04176f9d3820af8361bb8927fea3cee4
Johan Viklund <johan.viklund@nbis.se>
2021-03-13 13:29:31 +0100

    Separate clustering and stats calculations

--- cluster.pl
+++ cluster.pl
@@ -3,9 +3,10 @@ use strict;
 use warnings;
 
 use feature qw/ say /;
-use Time::Piece;
-
 use List::Util qw/ sum /;
+
+use Time::Piece;
+use Time::Seconds;
 
 my $CUTOFF = 0.34; # Valley from stats
 
@@ -44,53 +45,20 @@ if ( @current ) {
     @current = ();
 }
 
+my $start_day = localtime($times[0])->truncate(to => 'day')->epoch - ONE_DAY/2;
+my $next_day = $start_day + ONE_DAY;
 
-### Since it most probably starts with the weight down, the first instance is a
-### half lap. So we need to detect every "session" and then for every session
-### remove 0.5.
-my $DAY_CUTOFF = 3600;
-my $SESSION_CUTOFF = 1;
-
-my $tot_laps = 0;
-my $tot_seconds = 0;
-@current = ();
-my ($prev, $start);
-for my $t (@times) {
-    if ( ! @current ) {
-        @current = ($t);
-        $prev = $start = $t;
-        next;
-    }
-    my $cdiff = $t - $current[-1];
-    if ( $cdiff <= $SESSION_CUTOFF ) {
-        push @current, $t;
-        $prev = $t;
-        next;
-    }
-    elsif ( $cdiff > $DAY_CUTOFF ) {
-        display_stats();
-        $tot_laps = 0;
-        $tot_seconds = 0;
-        $start = $t;
-    }
-    elsif ( @current > 1 ) {
-        my $laps = @current - 0.5;
-        $tot_laps += $laps;
-        my $s = $current[$#current] - $current[0];
-        $tot_seconds += $s;
-    }
-    @current = ($t);
-    $prev = $t;
+if ( $times[0] > $next_day ) {
+    ($start_day, $next_day) = ($next_day, $next_day+ONE_DAY);
 }
 
-display_stats();
+open my $OUT, '>', localtime($start_day)->strftime("days/%Y%m%d.log");
 
-sub display_stats {
-    my $hours = int($tot_seconds/3600);
-    printf "%6.2f km (%5d %2d:%02d)  |  %s -- %s\n",
-        $tot_laps * 3.14 * 0.2 / 1000,
-        $tot_laps, $hours, ($tot_seconds-$hours*3600)/60,
-        Time::Piece->strptime(int($start), '%s')->datetime,
-        Time::Piece->strptime(int($prev), '%s')->datetime
-        if $tot_laps > 1
+for my $t (@times) {
+    if ( $t > $next_day ) {
+        ($start_day, $next_day) = ($next_day, $next_day+ONE_DAY);
+        close $OUT;
+        open $OUT, '>', localtime($start_day)->strftime("days/%Y%m%d.log");
+    }
+    say $OUT $t;
 }
---
+++ day_statistics.pl
@@ -0,0 +1,68 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use feature qw/ say /;
+
+### Since it most probably starts with the weight down, the first instance is a
+### half lap. So we need to detect every "session" and then for every session
+### remove 0.5.
+
+
+for my $file (glob('days/*.log')) {
+    open my $FILE, '<', $file or die "Could not read $file: $!, $?";
+    my ($date) = $file =~ m{^ days/ (\d+) \.log $}x;
+    my $stats = calculate($FILE);
+    display_stats($stats->{laps}, $stats->{seconds}, $date);
+}
+
+
+sub calculate {
+    my $FH = shift;
+    my $SESSION_CUTOFF = shift // 1;
+
+    my $tot_laps = 0;
+    my $tot_seconds = 0;
+    my @current = ();
+
+    my ($prev, $start);
+    while (my $t = <$FH>) {
+        #chomp($t);
+        if ( ! @current ) {
+            @current = ($t);
+            $prev = $start = $t;
+            next;
+        }
+        my $cdiff = $t - $current[-1];
+        if ( $cdiff <= $SESSION_CUTOFF ) {
+            push @current, $t;
+            $prev = $t;
+            next;
+        }
+        elsif ( @current > 1 ) {
+            my $laps = @current - 0.5;
+            $tot_laps += $laps;
+            my $s = $current[$#current] - $current[0];
+            $tot_seconds += $s;
+        }
+        @current = ($t);
+        $prev = $t;
+    }
+    return {
+        laps => $tot_laps,
+        seconds => $tot_seconds,
+    };
+}
+
+sub display_stats {
+    my ($tot_laps, $tot_seconds, $date) = @_;
+    my $hours = int($tot_seconds/3600);
+    printf "%6.2f km (%5d %2d:%02d)  |  %s\n",
+        $tot_laps * 3.14 * 0.2 / 1000,
+        $tot_laps, $hours, ($tot_seconds-$hours*3600)/60,
+        $date,
+        #Time::Piece->strptime(, '%s')->datetime,
+        #Time::Piece->strptime(int($prev), '%s')->datetime
+        #if $tot_laps > 1
+}
