1a18b3eef359bb9640aeede4a04eacbf6bb7501d
Johan Viklund <johan.viklund@nbis.se>
2021-03-15 19:10:37 +0000

    Faster plot data generation

--- day_plot.pl
+++ day_plot.pl
@@ -37,35 +37,46 @@ sub calculate {
         push @times, $t;
     }
 
+    return [] unless @times;
+
     my $tidx = 0;
     my $tp = $time->epoch;
     my $end = ($time + ONE_DAY)->epoch;
 
-    my @speed;
+    my @speed = ([$tp, 0]);
+    my @cluster = (0,0);
 
     while ( $tp < $end ) {
-        my $n = 0;
-
-        my $from = $tidx;
-        $n++ while --$from >= 0 && abs($times[$from] - $tp) < $WINDOW;
-        $from++;
-        $from = $from > $#times ? $#times : $from;
-
-        my $to = $tidx;
-        $n++ while ++$to <= $#times && abs($times[$to] - $tp) < $WINDOW;
-        $to--;
-        $to = $to > $#times ? $#times : $to;
-
-        my $speed = 0;
-        if ( $from != $to ) {
-            $speed = $n / ($times[$to] - $times[$from]);
-            $speed *= ONE_MINUTE;
+        ### Far to the future for next entry
+        if ( $times[$tidx] - $tp > $WINDOW+1 && ($tidx==0 || ($tp - $times[$tidx-1] > $WINDOW+1)) ) {
+            $tp = int( $times[$tidx] - $WINDOW );
+            @cluster = ($tidx,$tidx);
         }
 
-        push @speed, [ $tp, $speed ];
+        $cluster[0]++ while $cluster[0] >= 0 && $cluster[0] < $#times && $times[$cluster[0]] < $tp - $WINDOW;
+        $cluster[1]++ while $cluster[1] >= 0 && $cluster[1] < $#times && $times[$cluster[1]] < $tp + $WINDOW;
+        $cluster[0]-- while $cluster[0]>0 && $times[$cluster[0]] > $tp+$WINDOW;
+        $cluster[1]-- while $cluster[1]>0 && $times[$cluster[1]] > $tp+$WINDOW;
 
-        $tidx++ while $tidx <= $#times && $times[$tidx] < $tp;
+        my $speed = 0;
+        if ( $tp > $times[$cluster[0]]-1 && $tp < $times[$cluster[1]]+1 && $cluster[0] != $cluster[1] ) {
+            $speed = ($cluster[1] - $cluster[0])/($times[$cluster[1]] - $times[$cluster[0]]);
+            $speed *= ONE_MINUTE;
+            $speed = sprintf "%.0f", $speed;
+        }
+        if ( @speed>1 && $speed == $speed[-1][1] && $speed == $speed[-2][1] ) {
+            $speed[-1][0] = $tp;
+        }
+        else {
+            push @speed, [$tp, $speed];
+        }
+        $tidx++ while $tidx < $#times && $times[$tidx] < $tp;
         $tp++;
+
+        if ($tidx == $#times && $tp > $times[$tidx] && $speed == 0) {
+            push @speed, [$end, 0];
+            last;
+        }
     }
     return \@speed;
 }
