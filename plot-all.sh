bfe7f73243fca5ac14ffd8d3fdfa2df9a33b9671
Johan Viklund <johan.viklund@nbis.se>
2021-03-17 21:49:08 +0000

    Conditional plotting

--- plot-all.sh
+++ plot-all.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 for F in days/speed*.log; do
+    [ -f $F ] || continue
+    [ -s $F ] || continue
+
     echo "Processing $F"
     D=$(echo $F | sed 's/days\/speed-\([0-9]\+\).log/\1/')
     gnuplot -e "date='$D'" gnuplot.script
